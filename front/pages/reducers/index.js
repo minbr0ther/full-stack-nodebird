6c07c64e3a425003f49210ec800e8dd2b9bcc322
minbr0ther <minbr0ther@naver.com>
2022-03-20 16:14:33 +0900

    chore: redux-devtools 설치

--- front/pages/reducers/index.js
+++ front/pages/reducers/index.js
@@ -1,3 +1,5 @@
+import { HYDRATE } from 'next-redux-wrapper';
+
 const initialState = {
   user: {
     isLoggedIn: false,
@@ -25,6 +27,9 @@ export const logoutAction = () => {
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case HYDRATE:
+      console.log('HYDRATE', action);
+      return { ...state, ...action.payload };
     case 'LOG_IN':
       return {
         ...state,
@@ -43,6 +48,8 @@ const rootReducer = (state = initialState, action) => {
           user: null,
         },
       };
+    default:
+      return state;
   }
 };
 
--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,10 +1,16 @@
 import { createWrapper } from 'next-redux-wrapper';
-import { createStore } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-import reducer from '../reducers';
+import reducer from '../pages/reducers';
 
 const configureStore = () => {
-  const store = createStore(reducer);
+  const middlewares = [];
+  const enhancer = // 리덕스의 기능의 확장
+    process.env.NODE_ENV === 'production'
+      ? compose(applyMiddleware(...middlewares)) // 배포용
+      : composeWithDevTools(applyMiddleware(...middlewares)); // 개발용
+  const store = createStore(reducer, enhancer);
 
   return store;
 };
