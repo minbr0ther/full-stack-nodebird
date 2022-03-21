1e650963e1f2f35a6921c85085fed2463f4a541f
minbr0ther <minbr0ther@naver.com>
2022-03-21 20:14:52 +0900

    feat: post reducer 구현

---
+++ front/reducers/post.js
@@ -0,0 +1,69 @@
+export const initialState = {
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: '제로초',
+      },
+      content: '첫 번째 게시글 #해시태그 #익스프레스',
+      Images: [
+        {
+          src: 'https://picsum.photos/200/300',
+        },
+        {
+          src: 'https://picsum.photos/200/300',
+        },
+        {
+          src: 'https://picsum.photos/200/300',
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: 'nero',
+          },
+          content: '우와 개정판이 나왔군요~',
+        },
+        {
+          User: {
+            nickname: 'hero',
+          },
+          content: '얼른 사고싶어요~',
+        },
+      ],
+    },
+  ],
+  imagePaths: [], // 이미지들의 경로들이 저장될 예정
+  postAdded: false, // 게시글 추가가 완료되었을때 변경
+};
+
+const ADD_POST = 'ADD_POST';
+export const addPost = {
+  type: ADD_POST,
+};
+const dummyPost = {
+  id: 2,
+  content: '더미데이터입니다.',
+  User: {
+    id: 1,
+    nickname: '제로초',
+  },
+  Images: [],
+  Comments: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
