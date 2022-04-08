5d3f285d37bc4a73eb444f5e2a92616510e41902
minbr0ther <minbr0ther@naver.com>
2022-04-08 15:52:14 +0900

    feat: faker로 더미 데이터 생성

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import React from 'react';
+import Router from 'next/router';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import AppLayout from '../components/AppLayout';
 import FollowList from '../components/FollowList';
@@ -7,6 +8,16 @@ import NicknameEditForm from '../components/NicknameEditForm';
 
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push('/');
+    }
+  }, [me && me.id]);
+
+  if (!me) {
+    return null;
+  }
 
   return (
     <>
