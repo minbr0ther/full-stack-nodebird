150dbc449e3e14a29caa1b0c07a5457238894db1
minbr0ther <minbr0ther@naver.com>
2022-04-18 16:59:52 +0900

    feat: 작성한 글이 본인의 글이면 팔로우 버튼을 안보이게 한다

--- front/components/FollowButton.jsx
+++ front/components/FollowButton.jsx
@@ -25,6 +25,11 @@ const FollowButton = ({ post }) => {
       });
     }
   }, [isFollowing]);
+
+  // 작성한 글이 본인의 글이면 팔로우 버튼을 안보이게 한다.
+  // 이런 코드를 작성할 때는 hooks보다는 아래에 적어준다.
+  if (post.User.id === me.id) return null;
+
   return (
     <Button loading={followLoading || unfollowLoading} onClick={onClickButton}>
       {isFollowing ? '언팔로우' : '팔로우'}
